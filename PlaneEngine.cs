dd6d50ade43d04b5d3b1997ffbad9f53a85f790f
unknown-user <unknown-user@fe80::f129:2a3b:fc74:9b7d%12>
2015-04-26 14:20:12 +0200

    Initial check-in of module Assembly-CSharp

---
+++ PlaneEngine.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlaneEngine : MonoBehaviour {
+	private IAgility thruster;
+
+	// Use this for initialization
+	void Start () {
+		thruster = new PlaneAgility ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
+
+
+class PlaneAgility : IAgility {
+	public IList<Vector2> calculate(PowerChangePhase phase, Vector2 change) {
+		return new List<Vector2>();
+	}
+}
---
+++ vehicles/IAgility.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public interface IAgility {
+	
+	IList<Vector2> calculate (PowerChangePhase phase, Vector2 change);
+
+
+
+}
+
+public enum PowerChangePhase {
+	INC=1, CST=2, DEC=3
+}
